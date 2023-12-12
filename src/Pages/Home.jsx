523960bae08d9a84e1c08d733e4b322e437081d7
bsdev55 <bsasoumia55@yahoo.com>
2023-12-12 22:37:31 +0100

    fetch and display data from tvmaze api

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,10 @@
 import { useState } from 'react';
-import { Await } from 'react-router-dom';
+import { searchForShows } from '../api/tvmaze';
 
 function Home() {
   const [searchStr, setSearchStr] = useState('');
+  const [apiData, setApiData] = useState(null);
+  const [apiDataError, setApiDataError] = useState(null);
 
   const onSearchInputChange = (ev) => {
     setSearchStr(ev.target.value);
@@ -11,16 +13,39 @@ function Home() {
   const onSearch = async (ev) => {
     ev.preventDefault();
 
-    const response = await fetch("https://api.tvmaze.com/search/shows?q={searchStr}");
-    const body = await response.json();
-    console.log(body);
-  };
+    try {
+      setApiDataError(null);
+
+      const result = await searchForShows(searchStr);
+      setApiData(result);
+    } catch(error) {
+        setApiDataError(error);
+        console.log(apiDataError);
+      }
+    };
+
+    const renderApiData = () => {
+      if (apiDataError) {
+        //console.log(apiDataError.message);
+        return <div>Error occured: {apiDataError.message} </div>
+      }
+
+      if(apiData){
+        return apiData.map((data) => (
+          <div key={data.show.id}>{data.show.name}</div>
+        ));
+      }
+      return null;
+    }
   return (
     <div>
       <form onSubmit={onSearch}>
         <input type="text" value={searchStr} onChange={onSearchInputChange} />
         <button type="submit">Search</button>
       </form>
+      <div>
+        {renderApiData()}
+      </div>
     </div>
   );
 }
