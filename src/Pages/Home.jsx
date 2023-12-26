8d260b153edf1278d7f9f50a531481cce33698c0
bsdev55 <bsasoumia55@yahoo.com>
2023-12-26 22:47:46 +0100

    installed react-query. replace useShowById with useQuery hook

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -50,31 +50,6 @@ function Home() {
   return (
     <div>
       <SearchForm onSearch={onSearch} />
-      {/*
-      <form onSubmit={onSearch}>
-        <input type="text" value={searchStr} onChange={onSearchInputChange} />
-        <label>
-          Shows
-          <input
-            type="radio"
-            name="search-option"
-            value="shows"
-            checked={searchOption === 'shows'}
-            onChange={onRadioChange}
-          />
-        </label>
-        <label htmlFor="Actors">
-          Actors
-          <input
-            type="radio"
-            name="search-option"
-            value="actors"
-            checked={searchOption === 'actors'}
-            onChange={onRadioChange}
-          />
-        </label>
-        <button type="submit">Search</button>
-  </form>*/}
       <div>{renderApiData()}</div>
     </div>
   );
--- src/Pages/Show.jsx
+++ src/Pages/Show.jsx
@@ -1,9 +1,33 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
+import { getShowById } from "../api/tvmaze";
 
 const Show = () => {
     const { showId } = useParams();
+    const [showData, setShowData] = useState(null);
+    const [showError, setShowError] = useState(null);
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const data = await getShowById(showId);  
+                setShowData(data);              
+            } catch (error) {
+                setShowError(error);
+            }
+        }
+
+        fetchData();
+
+    }, [showId]);
+
+    if(showError) {
+        return <div>We have an error: {showError.message}</div>
+    }
+    if(showData) {
+        return <div>Got show data: {showData.name}</div>
+    }
     
-    return <div>Show Page for show {showId}</div>
+    return <div>Data is Loading {showId}</div>
 }
 
 export default Show
